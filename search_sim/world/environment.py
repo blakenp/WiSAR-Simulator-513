29ceb52623150acd83c9656318a6acc8bb54f249
Garrett Suggs <garrettdsuggs@gmail.com>
2026-02-26 19:41:52 -0700

    update environment.py to accept different numerical values for targets, and to accept indices and values for environmental hazards

--- search_sim/world/environment.py
+++ search_sim/world/environment.py
@@ -1,8 +1,10 @@
 """
     This class will implement the space we are searching.
 
-    target_indices: list of (x,y) coordinate pairs for a given target. For example, [[0,2],[4,4]] for two targets. Coordinates are given as
+    target/hazard_indices: list of (x,y) coordinate pairs for a given target/hazard. For example, [[0,2],[4,4]] for two targets. Coordinates are given as
     indices in an array.
+
+    target/hazard_values: a numerical score associated with a particular target/hazard. maybe useful for some kind of cost function.
 
     x/y_length: spatial length, probably in meters, of the x/y dimension of the space.
 
@@ -13,12 +15,11 @@
     x/y_size: length divided by num_pts. Useful for calculating distance traveled when traversing a cell.
 
     TODO:
-    - add capacity for hazards
     - add third dimension
 """
 
 class Environment:    
-    def __init__(self, target_indices, x_length=10, y_length=10, num_x_pts=10, num_y_pts=10):
+    def __init__(self, target_indices, target_values, hazard_indices, hazard_values, x_length=10, y_length=10, num_x_pts=10, num_y_pts=10):
         self.x_length = x_length
         self.y_length = y_length
         
@@ -29,10 +30,17 @@ class Environment:
 
         self.num_targets = len(target_indices)
 
-        for i in target_indices:
-            self.grid[i[1]][i[0]] = -1  # indexing is a little wonky, since python indexes rows first but rows match better
-                                        # with the y direction in my brain. the upshot is that if we want to be able to pass in coordinates
-                                        # as (x,y) pairs, then when we're accessing the grid we just need to flip the order and it's fine.
+        for i in range(len(target_values)):
+            x = target_indices[i][0]
+            y = target_indices[i][1]
+            self.grid[y][x] = target_values[i]  # indexing is a little wonky, since python indexes rows first but rows match better
+                                                # with the y direction in my brain. the upshot is that if we want to be able to pass in coordinates
+                                                # as (x,y) pairs, then when we're accessing the grid we just need to flip the order and it's fine.
+
+        for i in range(len(hazard_values)):
+            x = hazard_indices[i][0]
+            y = hazard_indices[i][1]
+            self.grid[y][x] = hazard_values[i]
         
         self.x_size = x_length/num_x_pts
         self.y_size = y_length/num_y_pts
