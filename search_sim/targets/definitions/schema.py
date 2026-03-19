0aaf612e3b4be5381308cd35478d4f672181b8e4
Garrett Suggs <garrettdsuggs@gmail.com>
2026-03-19 16:08:17 -0600

    add TargetState definition and functionality

--- search_sim/targets/definitions/schema.py
+++ search_sim/targets/definitions/schema.py
@@ -1,3 +1,6 @@
+from search_sim.agents.definitions.interfaces import Agent
+from search_sim.targets.definitions.interfaces import Target
+from search_sim.hazards.definitions.interfaces import Hazard
 from dataclasses import dataclass
 from enum import Enum
 
@@ -5,6 +8,7 @@ class TargetType(Enum):
     """Types of targets we want to simulate"""
 
     BASIC_TARGET = "basic_target"
+    RANDOM_TARGET = "random_target"
 
 @dataclass(frozen=True)
 class TargetState:
@@ -15,9 +19,34 @@ class TargetState:
         x: X position in grid cells.
         y: Y position in grid cells.
         value: Value of the target (e.g. importance, reward).
+        traversable_hazards: All hazard types the target can navigate through.
+        heading: Heading in degrees (0-360).
+        speed_mps: Current speed in meters per second.
+        awareness_radius: distance around the target within which it can perceive its surroundings.
+        nearby_agents: All agents within awareness_radius.
+        nearby_targets: All other targets within awareness_radius.
+        nearby_hazards: All hazards within awareness_radius.
     """
 
     id: str
     x: float
     y: float
     value: float
+    traversable_hazards: list[Hazard]
+    heading: float
+    speed_mps: float
+    awareness_radius: float
+    nearby_agents: list[Agent]
+    nearby_targets: list[Target]
+    nearby_hazards: list[Hazard]
+
+@dataclass(frozen=True)
+class TargetAction:
+    """The 'Intent' of a target for the next step.
+    
+    Attributes:
+        target_heading: Heading of where the target wants to move
+        target_speed: How fast the target wants to move to desired location
+    """
+    target_heading: float
+    target_speed: float
